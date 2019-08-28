a5a814695f4ecdc08254454d8990ef6c59464710
Bogdan Negura <id.bogdann@yahoo.com>
2019-08-28 22:36:44 +0100

    refactor using arrowFunction methode

--- app.js
+++ app.js
@@ -6,25 +6,19 @@ document.getElementById('button3').addEventListener('click', getExternal);
 // Get local text text file
 function getText() {
     fetch('text.txt')
-        .then(function(res){
-            return res.text()
-        })
-        .then(function(data) {
+        .then(res => res.text())
+        .then(data => {
             console.log(data);
             document.getElementById('output').innerHTML = data;
         })
-        .catch(function(err){
-            console.log(err);
-        });
+        .catch(err => console.log(err));
 }
 
 // Get local json data
 function getJson() {
     fetch('posts.json')
-        .then(function(res){
-            return res.json()
-        })
-        .then(function(data) {
+        .then(res => res.json())
+        .then(data => {
             console.log(data);
             let output = '';
             data.forEach(function(post) {
@@ -32,18 +26,14 @@ function getJson() {
             });
             document.getElementById('output').innerHTML = output;
         })
-        .catch(function(err){
-            console.log(err);
-        });
+        .catch(err => console.log(err));
 }
 
 // Get from external API
 function getExternal() {
     fetch('https://api.github.com/users')
-        .then(function(res){
-            return res.json()
-        })
-        .then(function(data) {
+        .then(res => res.json())
+        .then(data => {
             console.log(data);
             let output = '';
             data.forEach(function(user) {
@@ -51,7 +41,5 @@ function getExternal() {
             });
             document.getElementById('output').innerHTML = output;
         })
-        .catch(function(err){
-            console.log(err);
-        });
+        .catch(err => console.log(err));
 }
